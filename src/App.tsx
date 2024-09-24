d4cd52db35ba085e587317917d18b365becb892e
Lninn <thl384458870@outlook.com>
2024-09-24 15:05:09 +0800

    可以运行的demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,64 @@
 import './App.css'
 
+import { useRef, useState } from 'react'
+import { format } from 'date-fns'
+
 function App() {
+
+  const [running, setRunning] = useState(false)
+  const [inPause, setInPause] = useState(false)
+  const [milliseconds, setMilliseconds] = useState(0)
+
+  const timerIdRef = useRef(-1)
+
+  function onStart() {
+    if (!running) {
+      setMilliseconds(0)
+      setInPause(false)
+      
+      setRunning(true)
+      
+      working()
+    } else {
+      setRunning(false)
+      clearInterval(timerIdRef.current)
+    }
+  }
+
+  function onPause() {
+    if (!running) return
+
+    if (inPause) {
+      working(milliseconds)
+    } else {
+      clearInterval(timerIdRef.current)
+    }
+
+    setInPause(!inPause)
+  }
+
+  function working(initial = 0) {
+    let i = initial
+    const h = () => {
+      i = i + 1000
+      
+      setMilliseconds(i)
+    }
+    timerIdRef.current = setInterval(h, 100)
+  }
+
+  const currentTime = format(milliseconds, 'mm:ss')
+
   return (
     <>
-      hello
+      <p className='time'>{currentTime}</p>
 
-      <button>计时</button>
+      <div className='control-bar'>
+        <button onClick={onStart}>
+          {running ? '结束' : '开始'}
+        </button>
+        <button onClick={onPause} disabled={!running} >暂停</button>
+      </div>
     </>
   )
 }
--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,14 +1,15 @@
 import './Root.css'
 
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import { SolarSettingsLinear } from './SolarSettingsLinear'
 
 export default function Root() {
   return (
     <div>
       <header className='header'>
-        <h2>时间统计</h2>
-        <nav>
-          <Link to={'/'}>主页</Link>
+        <h2>计时器</h2>
+        <nav className='nav'>
+          <SolarSettingsLinear />
         </nav>
       </header>
       <main className='main-content'>
